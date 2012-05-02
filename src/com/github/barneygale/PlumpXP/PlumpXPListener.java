5659f91add18de6692b3aa39a74aa592da307234
lunchington <lordlunchington@gmail.com>
2012-05-02 14:48:09 -0400

    Rewrote the listener to detect how an entity dies, it seems to work properly.

--- src/com/github/barneygale/PlumpXP/PlumpXPListener.java
+++ src/com/github/barneygale/PlumpXP/PlumpXPListener.java
@@ -1,12 +1,15 @@
 package com.github.barneygale.PlumpXP;
 
-import org.bukkit.craftbukkit.entity.CraftPlayer;
+import org.bukkit.entity.Arrow;
+import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
-
-
+import org.bukkit.event.entity.PlayerDeathEvent;
 
 public class PlumpXPListener implements Listener {
 	public final PlumpXP plugin;
@@ -15,22 +18,61 @@ public class PlumpXPListener implements Listener {
 	}
 	@EventHandler
 	public void onEntityDeath(EntityDeathEvent event) {
-		double xp = event.getDroppedExp();
-		EntityType etype = event.getEntityType();
-		
-		if(etype == EntityType.PLAYER) {
-			if(plugin.config.PLAYER_OVERRIDE) {
-				xp = ((CraftPlayer)event.getEntity()).getTotalExperience();
-				xp*= plugin.config.PLAYER_MULTIPLIER;
-			}
-		
-		} else {
-			if(etype == EntityType.BLAZE) {
-				xp*=plugin.config.BLAZE_MULTIPLIER;
-			}
-			xp*=plugin.config.PLUMP_MULTIPLIER;
+		 Entity target = event.getEntity();
+	     Player attacker = getAttacker(target.getLastDamageCause());
+	     int xp = event.getDroppedExp();
+	     
+		 if(event instanceof PlayerDeathEvent) {
+	            handlePlayerDeath((PlayerDeathEvent) event,attacker, xp);
+	            return;
+	     }
+	
+		 //not killed by player
+	     if(attacker == null) {
+	            return;
+	        }
+	     	handleMonsterDeath(event, attacker, xp);
 		}
-		event.setDroppedExp((int)xp);
+	
+	private void handleMonsterDeath(EntityDeathEvent event, Player attacker, int exp) {
+		EntityType target = event.getEntityType();
+				
+		if (target == EntityType.BLAZE) {
+			exp *= plugin.config.BLAZE_MULTIPLIER;
+		}
+		else {
+			exp *= plugin.config.PLUMP_MULTIPLIER;
+		}
+		event.setDroppedExp(exp);
 	}
+	
+	private void handlePlayerDeath(PlayerDeathEvent event,Player attacker, int exp) {
+		Player target = (Player)event.getEntity();	
+		Player killer = getAttacker(target.getLastDamageCause());
+		//killed by another player
+		if (killer != null) {
+			exp *= plugin.config.PLAYER_MULTIPLIER;
+		}
+		event.setDroppedExp(exp);
+	}
+	
+	private Player getAttacker(EntityDamageEvent attacker) {
+        if((attacker == null) || (!(attacker instanceof EntityDamageByEntityEvent))){
+            return null;
+        }
+   
+        Player p = null;
+        Entity damager = ((EntityDamageByEntityEvent) attacker).getDamager();
+
+        if(damager instanceof Arrow) {
+            Arrow damage_arrow = (Arrow) damager;
+            if(damage_arrow.getShooter() instanceof Player) {
+                p = (Player) damage_arrow.getShooter();
+            }
+        } else if(damager instanceof Player) {
+            p = (Player)damager;
+        }
+        return p;
+    }
 	
 }
