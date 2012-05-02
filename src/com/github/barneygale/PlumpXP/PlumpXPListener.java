0f64d5595294113a149e29575420c540b512e38d
lunchington <lordlunchington@gmail.com>
2012-05-02 14:56:45 -0400

    Fixing formatting <3 Deaygo

--- src/com/github/barneygale/PlumpXP/PlumpXPListener.java
+++ src/com/github/barneygale/PlumpXP/PlumpXPListener.java
@@ -12,51 +12,51 @@ import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 
 public class PlumpXPListener implements Listener {
-	public final PlumpXP plugin;
-	public PlumpXPListener(PlumpXP instance) {
-		plugin = instance;
-	}
-	@EventHandler
-	public void onEntityDeath(EntityDeathEvent event) {
-		 Entity target = event.getEntity();
-	     Player attacker = getAttacker(target.getLastDamageCause());
-	     int xp = event.getDroppedExp();
-	     
-		 if(event instanceof PlayerDeathEvent) {
-	            handlePlayerDeath((PlayerDeathEvent) event,attacker, xp);
-	            return;
-	     }
-	
-		 //not killed by player
-	     if(attacker == null) {
-	            return;
-	        }
-	     	handleMonsterDeath(event, attacker, xp);
-		}
-	
-	private void handleMonsterDeath(EntityDeathEvent event, Player attacker, int exp) {
-		EntityType target = event.getEntityType();
-				
-		if (target == EntityType.BLAZE) {
-			exp *= plugin.config.BLAZE_MULTIPLIER;
-		}
-		else {
-			exp *= plugin.config.PLUMP_MULTIPLIER;
-		}
-		event.setDroppedExp(exp);
-	}
-	
-	private void handlePlayerDeath(PlayerDeathEvent event,Player attacker, int exp) {
-		Player target = (Player)event.getEntity();	
-		Player killer = getAttacker(target.getLastDamageCause());
-		//killed by another player
-		if (killer != null) {
-			exp *= plugin.config.PLAYER_MULTIPLIER;
-		}
-		event.setDroppedExp(exp);
-	}
-	
-	private Player getAttacker(EntityDamageEvent attacker) {
+    public final PlumpXP plugin;
+    public PlumpXPListener(PlumpXP instance) {
+        plugin = instance;
+    }
+    @EventHandler
+    public void onEntityDeath(EntityDeathEvent event) {
+         Entity target = event.getEntity();
+         Player attacker = getAttacker(target.getLastDamageCause());
+         int xp = event.getDroppedExp();
+         
+         if(event instanceof PlayerDeathEvent) {
+                handlePlayerDeath((PlayerDeathEvent) event,attacker, xp);
+                return;
+         }
+    
+         //not killed by player
+         if(attacker == null) {
+                return;
+            }
+             handleMonsterDeath(event, attacker, xp);
+        }
+    
+    private void handleMonsterDeath(EntityDeathEvent event, Player attacker, int exp) {
+        EntityType target = event.getEntityType();
+                
+        if (target == EntityType.BLAZE) {
+            exp *= plugin.config.BLAZE_MULTIPLIER;
+        }
+        else {
+            exp *= plugin.config.PLUMP_MULTIPLIER;
+        }
+        event.setDroppedExp(exp);
+    }
+    
+    private void handlePlayerDeath(PlayerDeathEvent event,Player attacker, int exp) {
+        Player target = (Player)event.getEntity();    
+        Player killer = getAttacker(target.getLastDamageCause());
+        //killed by another player
+        if (killer != null) {
+            exp *= plugin.config.PLAYER_MULTIPLIER;
+        }
+        event.setDroppedExp(exp);
+    }
+    
+    private Player getAttacker(EntityDamageEvent attacker) {
         if((attacker == null) || (!(attacker instanceof EntityDamageByEntityEvent))){
             return null;
         }
@@ -74,5 +74,5 @@ public class PlumpXPListener implements Listener {
         }
         return p;
     }
-	
+    
 }
